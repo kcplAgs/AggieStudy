6e7a65a4baf83ba7586980f9e8955a954c58a3b6
Cam <cameron28202@gmail.com>
2024-06-20 22:11:46 +0200

    Added link to each resource

--- Frontend (ReactJS)/src/App.js
+++ Frontend (ReactJS)/src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Courses from './pages/Courses';
 import Navbar from './components/Navbar';
 import About from "./pages/About";
+import Resources from "./pages/Resources";
+import ResourceDetail from "./components/ResourceDetail"
 
 const App = () => {
     return (
@@ -13,6 +15,8 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/courses" element={<Courses />} />
                 <Route path="/about" element={<About/>}/>
+                <Route path="/resources/:classId" component={Resources}></Route>
+                <Route path="/resources/:resources" component={ResourceDetail}></Route>
             </Routes>
         </Router>
     );
